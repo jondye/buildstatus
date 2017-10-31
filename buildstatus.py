4f32a69ff926228d38b1f5c9ad9be85ff8ac79aa
Jon Dye <jon@pecorous.co.uk>
2017-10-31 21:41:15 +0000

    Initial build status implementation

--- buildstatus.py
+++ buildstatus.py
@@ -1,6 +1,54 @@
 #!python3
+
+import os
 import time
 
-while True:
-    print("Hello world")
-    time.sleep(30)
+from jenkins import Jenkins
+from gpiozero import StatusBoard
+
+
+def main():
+    delay = os.environ.get('POLL_PERIOD', 60)
+    server = os.environ['JENKINS_URI']
+    j = Jenkins(server)
+    status = StatusBoard(pwm=True)
+    job_names = [os.environ.get('JENKINS_JOB_%d' % i+1) for i in range(5)]
+    job_color = [None for _ in job_names]
+
+    while True:
+        for i, name in enumerate(job_names):
+            lights = status[i].lights
+            if not name:
+                lights.off()
+                continue
+
+            job = j.job(name)
+            if not job.enabled:
+                lights.off()
+                continue
+
+            color = job.info['color']
+            if color == job_color[i]:
+                continue
+
+            if color.startswith('blue'):
+                lights.red.off()
+                if color.endswith('_anime'):
+                    lights.green.pulse()
+                else:
+                    lights.green.on()
+            elif color.startswith('red'):
+                lights.green.off()
+                if color.endswith('_anime'):
+                    lights.red.pulse()
+                else:
+                    lights.red.on()
+            else:
+                lights.on()
+            job_color = color
+
+        time.sleep(delay)
+
+
+if __name__ == '__main__':
+    main()
