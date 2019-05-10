03677d9974275c49036b99bbfd02ba43541e44d2
Jon Dye <jon@pecorous.co.uk>
2019-05-10 17:01:51 +0100

    Create package for buildstatus

---
+++ buildstatus/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name="buildstatus",
+    version="0.1",
+    scripts=['buildstatus.py'],
+    install_requires=['gpiozero', 'rpi.gpio', 'jenkins-webapi'],
+)
