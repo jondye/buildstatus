1263313d97f5cb82bd89fc99c1bb669ec59ed3a2
Jon Dye <jon@pecorous.co.uk>
2020-03-26 15:34:48 +0000

    Install as buildstatus

--- buildstatus/setup.py
+++ buildstatus/setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name="buildstatus",
     version="0.1",
-    scripts=['buildstatus.py'],
     install_requires=['gpiozero', 'jenkins-webapi', 'sshtunnel'],
+    py_modules=['buildstatus'],
+    entry_points={"console_scripts":["buildstatus=buildstatus:main"]}
 )
